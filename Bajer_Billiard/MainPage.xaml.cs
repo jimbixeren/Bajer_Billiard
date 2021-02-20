3e66387fe01a50c40a9752b4d27de0e4fce113a5
Ben Johnson <jimbixeren@gmail.com>
2021-02-20 21:23:01 +0100

    Add project files.

---
+++ Bajer_Billiard/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Bajer_Billiard.Players;
+
+
+namespace Bajer_Billiard
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var assembly = typeof(MainPage);
+
+            iconImage.Source = ImageSource.FromResource("Bajer_Billiard.Assets.Images.DeadPool.png", assembly);
+
+          
+            
+
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Bajer_Billiard/Players/Players.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Bajer_Billiard.Players
+{
+    public class Players
+    {
+        string Name;
+        int playersNum;
+        int pointLavet;
+        int point;
+        int gode;
+        int res;
+        int skævecount;
+        int skæve;
+
+       
+        public string Deltager()
+        {
+            return playersNum.ToString();
+            return Name.ToUpper();
+
+        }
+
+
+
+
+        public string Start()
+        {
+
+            res = 60;
+           
+            return res.ToString();
+
+        }
+
+        public string Gode()
+        {
+
+            res = point - gode;
+            return res.ToString();
+
+
+        }
+
+        public string Skæve()
+        {
+            skævecount = skæve;
+            return skævecount.ToString();
+
+
+        }
+
+
+
+
+    }
+}
